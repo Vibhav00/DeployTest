7ec6eaf6ebf2e61dbcd34b024f3fb9efe26c7007
Vibhav <vibhavkumarmishra00170@gmail.com>
2024-05-14 15:13:29 +0530

    nodemodel

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = require('./app')
 // database connection
 
 // starting server
+
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is working on http://127.0.0.1:5000`)
 })
